2d032e4a126afdf66830170a1514a0d01534ba13
Colin Campbell <colin@elusivelabs.com>
2017-09-18 22:45:03 -0400

    Add simple ssr

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const renderer = require('vue-server-renderer').createRenderer();
+const Vue = require('vue');
 
 const server = express();
 
@@ -8,6 +10,30 @@ server.use(helmet());
 server.use(morgan('combined'));
 
 const PORT = 3000;
+
+server.get('*', (req, res) => {
+    const app = new Vue({
+      data: {
+        url: req.url
+      },
+      template: `<div>The visited URL is: {{ url }}</div>`
+    })
+  
+    renderer.renderToString(app, (err, html) => {
+      if (err) {
+        res.status(500).end('Internal Server Error')
+        return
+      }
+      res.send(`
+        <!DOCTYPE html>
+        <html lang="en">
+          <head><title>Hello</title></head>
+          <body>${html}</body>
+        </html>
+      `)
+    })
+  })
+  
 
 server.listen(PORT, function () {
     console.log('listening on port ' + PORT);
