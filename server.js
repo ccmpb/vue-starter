fbf3eb86f595fb98ec6f352f44630510e06ea455
Colin Campbell <colin@elusivelabs.com>
2017-09-19 21:15:08 -0400

    use html template

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
-const renderer = require('vue-server-renderer').createRenderer();
+const createRenderer = require('vue-server-renderer').createRenderer;
 const Vue = require('vue');
 
 const server = express();
@@ -11,29 +11,28 @@ server.use(morgan('combined'));
 
 const PORT = 3000;
 
+const renderer = createRenderer({
+    template: require('fs').readFileSync('./src/templates/index.template.html', 'utf-8')
+});
+
 server.get('*', (req, res) => {
     const app = new Vue({
-      data: {
-        url: req.url
-      },
-      template: `<div>The visited URL is: {{ url }}</div>`
+        data: {
+            url: req.url
+        },
+        template: `<div>The visited URL is: {{ url }}</div>`
     })
-  
+
     renderer.renderToString(app, (err, html) => {
-      if (err) {
-        res.status(500).end('Internal Server Error')
-        return
-      }
-      res.send(`
-        <!DOCTYPE html>
-        <html lang="en">
-          <head><title>Hello</title></head>
-          <body>${html}</body>
-        </html>
-      `)
+        if (err) {
+            res
+                .status(500)
+                .end('Internal Server Error')
+            return
+        }
+        res.send(html);
     })
-  })
-  
+})
 
 server.listen(PORT, function () {
     console.log('listening on port ' + PORT);
