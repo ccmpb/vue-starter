9e2fbf1c07775b8aa715e85d893334e63fe516a5
Colin Campbell <colin@elusivelabs.com>
2017-09-30 04:45:18 -0400

    Add less loader

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -18,6 +18,16 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "less-loader" // compiles Less to CSS
+                }]
             }
         ]
     }
