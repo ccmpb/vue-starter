e4319e8d3c2693287af22d7a2c2b3b04f07e702e
Colin Campbell <colin@elusivelabs.com>
2017-09-30 04:42:01 -0400

    Add vue & babel loaders

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,9 +1,24 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/client.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '../dist')
-  }
+    entry: './src/client.js',
+    devtool: 'cheap-module-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }, 
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
 };
